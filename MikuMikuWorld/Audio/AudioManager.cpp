521fcc0a787f045a6e078e3036ef288e26c0b15a
Crash5b <crash5band@gmail.com>
2022-09-14 15:34:18 +0200

    Reduce memory usage when loading music files
    Stream bgm using MA_SOUND_FLAG_STREAM

--- MikuMikuWorld/Audio/AudioManager.cpp
+++ MikuMikuWorld/Audio/AudioManager.cpp
@@ -89,7 +89,12 @@ namespace MikuMikuWorld
 		if (!std::filesystem::exists(wFilename))
 			return false;
 
-		ma_uint32 flags = MA_SOUND_FLAG_NO_PITCH | MA_SOUND_FLAG_NO_SPATIALIZATION | MA_SOUND_FLAG_DECODE | MA_SOUND_FLAG_ASYNC;
+		ma_uint32 flags = MA_SOUND_FLAG_NO_PITCH
+			| MA_SOUND_FLAG_NO_SPATIALIZATION
+			| MA_SOUND_FLAG_DECODE
+			| MA_SOUND_FLAG_ASYNC
+			| MA_SOUND_FLAG_STREAM;
+
 		ma_result bgmResult = ma_sound_init_from_file_w(&engine, wFilename.c_str(), flags, &bgmGroup, NULL, &bgm);
 		if (bgmResult != MA_SUCCESS)
 		{
