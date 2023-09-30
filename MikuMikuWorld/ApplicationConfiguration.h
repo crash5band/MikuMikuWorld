dcf811c2f96f373413c0b395e68ee9439e70e6e5
Sherif <crash5band@gmail.com>
2023-09-30 21:16:30 +0300

    Update default keyboard shortcuts

--- MikuMikuWorld/ApplicationConfiguration.h
+++ MikuMikuWorld/ApplicationConfiguration.h
@@ -44,10 +44,10 @@ namespace MikuMikuWorld
 		MultiInputBinding timelineHoldMid = { "timeline_hold_step", {ImGuiKey_4}, {ImGuiKey_Keypad4} };
 		MultiInputBinding timelineFlick = { "timeline_flick", {ImGuiKey_5}, {ImGuiKey_Keypad5} };
 		MultiInputBinding timelineCritical = { "timeline_critical", {ImGuiKey_6}, {ImGuiKey_Keypad6} };
-		MultiInputBinding timelineFriction = { "timeline_trace", {} };
-		MultiInputBinding timelineBpm = { "timeline_bpm", {ImGuiKey_7}, {ImGuiKey_Keypad7} };
-		MultiInputBinding timelineTimeSignature = { "timeline_time_signature", {ImGuiKey_8}, {ImGuiKey_Keypad8} };
-		MultiInputBinding timelineHiSpeed = { "timeline_hi_speed", {ImGuiKey_9}, {ImGuiKey_Keypad9} };
+		MultiInputBinding timelineFriction = { "timeline_trace", {ImGuiKey_7}, {ImGuiKey_Keypad7} };
+		MultiInputBinding timelineBpm = { "timeline_bpm", {ImGuiKey_8}, {ImGuiKey_Keypad8} };
+		MultiInputBinding timelineTimeSignature = { "timeline_time_signature", {ImGuiKey_9}, {ImGuiKey_Keypad9} };
+		MultiInputBinding timelineHiSpeed = { "timeline_hi_speed", {ImGuiKey_0}, {ImGuiKey_Keypad0} };
 	};
 
 	struct ApplicationConfiguration
