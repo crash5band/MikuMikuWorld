c59eb05d622ec287259f13653588ed14f9acad21
Sherif <crash5band@gmail.com>
2025-09-12 22:51:47 +0300

    Change particles texture filter mode

--- MikuMikuWorld/Application.cpp
+++ MikuMikuWorld/Application.cpp
@@ -325,7 +325,7 @@ namespace MikuMikuWorld
 		ResourceManager::loadTexture(texturesDir + "timeline_tools.png");
 		ResourceManager::loadTexture(texturesDir + "note_stats.png");
 		ResourceManager::loadTexture(texturesDir + "stage.png");
-		ResourceManager::loadTexture(texturesDir + "particles.png", TextureFilterMode::LinearMipMapLinear, TextureFilterMode::LinearMipMapLinear);
+		ResourceManager::loadTexture(texturesDir + "particles.png", TextureFilterMode::LinearMipMapLinear, TextureFilterMode::Linear);
 
 		ResourceManager::loadTransforms(texturesDir + "transform.txt");
 		ResourceManager::loadParticleEffects(texturesDir + "particles.pte");
